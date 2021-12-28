6088c3413533c881d35efb42206c6c395776e8ee
duy.leduc8 <duy.leduc8@gmail.com>
2021-12-28 22:20:28 +0700

    apply SSL to local server

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
+const fs = require('fs');
+const path = require('path');
 const express = require("express");
 const app = express();
-const { createServer } = require('http');
-const httpServer = createServer(app);
+const { createServer } = require('https');
+const httpsServer = createServer({
+  key: fs.readFileSync(path.join(__dirname, 'cert', 'key.pem')),
+  cert: fs.readFileSync(path.join(__dirname, "cert", "cert.pem"))
+}, app);
 const { Server } = require("socket.io");
-const io = new Server(httpServer, {
+const io = new Server(httpsServer, {
   cors: {
     origin: "*"
   }
@@ -17,6 +22,6 @@ require("./startup/validation")();
 require("./events")(io);
 
 const port = process.env.PORT || 3000;
-httpServer.listen(port, () => {
+httpsServer.listen(port, () => {
   console.log(`Listening on port ${port}...`);
 });
